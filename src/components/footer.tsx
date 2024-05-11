5131878ccc78449fa27ac7cbb4b68c01b141e938
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-05-11 17:23:13 +0300

    container props (ReactNode)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,7 +14,9 @@ const routes = [
 export default function Footer() {
   return (
     <footer className="mt-auto flex items-center justify-between h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25">
-      <smal className="text-xs">&copy; 2050 ByteGrad. All rights reserved</smal>
+      <small className="text-xs">
+        &copy; 2050 ByteGrad. All rights reserved
+      </small>
 
       <ul className="flex gap-x-3 sm:gap-x-8">
         {routes.map((route) => (
