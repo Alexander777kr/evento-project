7926b31badba8826948b7d44b366f1ade4d50c48
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-05-17 17:44:08 +0300

    display event name as title (generateMetadata)

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,31 +1,35 @@
 import EventsList from '@/components/events-list';
 import H1 from '@/components/h1';
-import { EventoEvent } from '@/lib/types';
-import { log } from 'console';
+import { Suspense } from 'react';
+import Loading from './loading';
+import { Metadata } from 'next';
+import { capitalize } from '@/lib/utils';
 
-type EventsPageProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-export default async function EventsPage({ params }: EventsPageProps) {
+export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
+  return {
+    title: city === 'all' ? 'All Events' : `Events in ${capitalize(city)}`,
+  };
+}
 
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-
-  const events: EventoEvent[] = await response.json();
+export default async function EventsPage({ params }: Props) {
+  const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === 'all' && 'All Events'}
-        {city !== 'all' &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== 'all' && `Events in ${capitalize(city)}`}
       </H1>
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
